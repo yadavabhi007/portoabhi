7cf485fe529b525ab82aa70d3a3b32742a8cd6e3
Abhishek Yadav <yadav31abhi@gmail.com>
2023-05-30 10:26:18 +0530

    updated

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import *
 from django import forms
-from django.contrib.auth import authenticate, get_user_model, password_validation
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm, UserChangeForm as  BaseUserChangeForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
-from django.utils.translation import gettext_lazy as _
 
 
 admin.site.site_title = "Abhishek Yadav"
@@ -13,23 +11,21 @@ admin.site.site_header = "Abhishek Yadav Administration"
 admin.site.index_title = "Abhishek Yadav Administration"
 
 
-class CountryCodeFormCreate(BaseUserCreationForm):
+class UserCreationForm(BaseUserCreationForm):
     class Meta:
         widgets = {
             'mobile_number': PhoneNumberPrefixWidget(initial='IN'),
         }
 
-
-class CountryCodeFormUpdate(BaseUserChangeForm):
+class UserChangeForm(BaseUserChangeForm):
     class Meta:
         widgets = {
             'mobile_number': PhoneNumberPrefixWidget(initial='IN'),
         }
-
 
 class UserModelAdmin(BaseUserAdmin):
-    form = CountryCodeFormUpdate
-    add_form = CountryCodeFormCreate
+    form = UserChangeForm
+    add_form = UserCreationForm
     list_display = ('id', 'username', 'email', 'mobile_number', 'is_active', 'action', 'created_at', 'updated_at')
     list_filter = ('is_active', 'is_staff', 'is_superuser', 'groups', 'created_at', 'updated_at')
     fieldsets = (
@@ -47,7 +43,9 @@ class UserModelAdmin(BaseUserAdmin):
     search_fields = ('username', 'email', 'mobile_number', 'first_name', 'last_name')
     ordering = ('id', 'username', 'email', 'mobile_number', 'first_name', 'last_name', 'created_at', 'updated_at')
     list_per_page = 20
+    list_max_show_all = 10000000
     filter_horizontal = ()
+    filter_vertical = ()
     readonly_fields = ('last_login', 'created_at', 'updated_at')
 
     def action(self, obj):
@@ -58,3 +56,15 @@ class UserModelAdmin(BaseUserAdmin):
             social_button = '<a  href="#">---</a>'
             return mark_safe(u''.join(social_button))
 admin.site.register(User, UserModelAdmin)
+
+
+# class AdminContactForm(forms.ModelForm):
+#     class Meta:
+#         widgets = {
+#             'phone': PhoneNumberPrefixWidget(initial='IN'),
+#         }
+
+# @admin.register(Contact)
+# class ContactAdmin(admin.ModelAdmin):
+#     form = AdminContactForm
+
