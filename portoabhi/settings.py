59cf4e6d067aa24e260ff4809ea618997f66fa72
Abhishek Yadav <yadav31abhi@gmail.com>
2023-08-20 13:06:37 +0530

    updated

--- portoabhi/settings.py
+++ portoabhi/settings.py
@@ -154,8 +154,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mobappssolutions147@gmail.com'
-EMAIL_HOST_PASSWORD = 'eyagiorzjysugiak'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
